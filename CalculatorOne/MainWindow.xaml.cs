a89bd2d4ee6bdbc16dffb55418e8a25a08f33ddd
germanmsh <34372414+germanmsh@users.noreply.github.com>
2017-12-10 02:37:20 +1100

    Button add void

--- CalculatorOne/MainWindow.xaml.cs
+++ CalculatorOne/MainWindow.xaml.cs
@@ -29,13 +29,13 @@ namespace CalculatorOne
         bool bExist;
         int a;
         int b;
-        int q;
+        bool q;
 
         public void StartCalc()
         {
             a = 0;
             b = 0;
-            q = 0;
+            q = false;
             aString = "";
             bExist = false;
             TextBox1.Text = "0";
@@ -44,6 +44,7 @@ namespace CalculatorOne
         private void Button_Click(object sender, RoutedEventArgs e)
         {
                 aString = aString + "1";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -54,57 +55,107 @@ namespace CalculatorOne
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            aString = aString + "2";
+            aString = aString + '2';
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             aString = aString + "3";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
             aString = aString + "4";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
             aString = aString + "5";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
             aString = aString + "6";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
             aString = aString + "7";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
             aString = aString + "8";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
             aString = aString + "9";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_10(object sender, RoutedEventArgs e)
         {
             aString = aString + "0";
+            TextBox1.Text = Convert.ToString(aString);
         }
 
         private void Button_Click_11(object sender, RoutedEventArgs e)
         {
-            TextBox1.Text = Convert.ToString(a + b);
+            b = Convert.ToInt32(aString);
+            if (q)
+            {
+                TextBox1.Text = Convert.ToString(a + b);
+                aString = Convert.ToString(a + b);
+            }
+            else
+            {
+                TextBox1.Text = Convert.ToString(a - b);
+                aString = Convert.ToString(a - b);
+            }
+            bExist = false;
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
-            q = 1;
+            q = false;
+            if (bExist)
+            {
+                b = Convert.ToInt32(aString);
+                a = a - b;
+                TextBox1.Text = Convert.ToString(a);
+            }
+            else
+            {
+                a = Convert.ToInt32(aString);
+                aString = "";
+                bExist = true;
+            }
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            q = true;
+            if (bExist)
+            {
+                b = Convert.ToInt32(aString);
+                a = a + b;
+                TextBox1.Text = Convert.ToString(a);
+            }
+            else
+            {
+                a = Convert.ToInt32(aString);
+                aString = "";
+                bExist = true;
+            }
+
         }
     }
 }
