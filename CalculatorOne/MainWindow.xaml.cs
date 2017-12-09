b67798f5ebb7aa47d9ec1def3af1a27d9991823b
germanmsh <34372414+germanmsh@users.noreply.github.com>
2017-12-10 02:08:42 +1100

    Button added code

--- CalculatorOne/MainWindow.xaml.cs
+++ CalculatorOne/MainWindow.xaml.cs
@@ -19,10 +19,92 @@ namespace CalculatorOne
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {     
         public MainWindow()
         {
             InitializeComponent();
+            StartCalc();
+        }
+        string aString;
+        bool bExist;
+        int a;
+        int b;
+        int q;
+
+        public void StartCalc()
+        {
+            a = 0;
+            b = 0;
+            q = 0;
+            aString = "";
+            bExist = false;
+            TextBox1.Text = "0";
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                aString = aString + "1";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            StartCalc();
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "2";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "3";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "4";
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "5";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "6";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "7";
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "8";
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "9";
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            aString = aString + "0";
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = Convert.ToString(a + b);
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            q = 1;
         }
     }
 }
